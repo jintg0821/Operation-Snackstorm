eb359172c7b276217349e2662d377930d939a9c5
fti11 <mxonstruck@gmail.com>
2025-12-07 00:36:53 +0900

    .

--- Operation-Snackstorm/Assets/Scripts/GameManager.cs
+++ Operation-Snackstorm/Assets/Scripts/GameManager.cs
@@ -68,6 +68,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     void Start()
     {
+        timerTime = 90f;
         SetPlayerPosition();
 
         if (timerText != null && roundText != null)
